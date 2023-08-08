160713d09b6abb4ed8cb81972cc8b2b257a0d9bb
francisallin <lamtszto1997@gmail.com>
2023-08-08 12:30:00 -0400

    add delete from cart popup & zuckerberg jpg

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -263,7 +263,7 @@ module.exports.performDelete = async (req, res, next) => {
   )
     .then((order) => {
       // console.log(order);
-      res.redirect("/cart");
+      res.redirect("/cart?deleteFromCartSuccess=true")
     })
     .catch((err) => {
       res.status(500).send({
